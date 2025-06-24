3fbd20313c277395a4e1fbdd7fca8316050984ad
Saurabh Jain <saurabhkumarjain@Saurabhs-MacBook-Pro.local>
2025-06-24 16:14:14 +0530

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/saurabh-jain/', // 👈 important
   plugins: [react()],
-})
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
